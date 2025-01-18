53f6a3be59a7b365bd3785ba318dfd92e3dbce67
Daniel Fedotov <dfeed88@proton.me>
2025-01-18 10:34:01 +0100

    Delete scripts directory

--- scripts/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('[FEDOTOV.DEV] Script status: LOADED!');
-
-    const avatar = document.getElementById('avatar');
-    avatar.addEventListener('click', () => {
-        alert('print("Hello, world!")');
-        console.log(';)');
-    });
-});
